477ab2a448dedb0aa35adc6b81b3fadf5fdd6ebd
Omar Sakka amini <omarsakkaamini@gmail.com>
2023-08-16 04:20:15 +0300

    better erroe handling

--- src/container/App.js
+++ src/container/App.js
@@ -4,7 +4,7 @@ import { robots } from '../robots';
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
-
+import ErrorBoundry from '../components/ErrorBoundry';
 class App extends Component {
     constructor(){
         super()
@@ -34,7 +34,9 @@ class App extends Component {
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox SearchChange={this.onSearchChange}/>
           <Scroll>
+            <ErrorBoundry>
           <CardList robots={filteredRobots}/> 
+           </ErrorBoundry>
           </Scroll>
           </div>
         )}
